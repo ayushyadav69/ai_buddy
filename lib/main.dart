4cec0e850e54e29378a694fb4880f5cc40d8d25e
Ayush Yadav <ayush.dms.1415@gmail.com>
2026-05-21 09:59:02 +0530

    Initial Flutter Firebase Setup

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:ai_buddy/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Hive.initFlutter();
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Buddy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('AI Buddy Setup Done', style: TextStyle(fontSize: 22)),
+        ),
+      ),
+    );
+  }
+}
