09bbfc114ed01f589806fbc9964553d224dfe681
Ayush Yadav <ayush.dms.1415@gmail.com>
2026-05-27 11:54:56 +0530

    redesigned buddy home cards

--- lib/features/buddy/presentation/widgets/buddy_home_card.dart
+++ lib/features/buddy/presentation/widgets/buddy_home_card.dart
@@ -1,6 +1,8 @@
 import 'package:ai_buddy/features/buddy/domain/entities/buddy_definition.dart';
-import 'package:flutter/material.dart';
+import 'package:ai_buddy/features/buddy/presentation/theme/buddy_visual_theme.dart';
 import 'package:ai_buddy/features/buddy/presentation/widgets/buddy_rive_view.dart';
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart' as rive;
 
 class BuddyHomeCard extends StatelessWidget {
   final BuddyDefinition buddy;
@@ -10,66 +12,124 @@ class BuddyHomeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeColor = _themeColor;
+    final visualTheme = buddy.visualTheme;
 
     return Material(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(28),
-      elevation: 4,
+      color: Colors.transparent,
       child: InkWell(
         onTap: onTap,
-        borderRadius: BorderRadius.circular(28),
-        child: Padding(
-          padding: const EdgeInsets.all(14),
-          child: Column(
+        borderRadius: BorderRadius.circular(34),
+        child: Ink(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [visualTheme.primaryColor, visualTheme.secondaryColor],
+            ),
+            borderRadius: BorderRadius.circular(34),
+            boxShadow: [
+              BoxShadow(
+                color: visualTheme.primaryColor.withValues(alpha: 0.28),
+                blurRadius: 18,
+                offset: const Offset(0, 10),
+              ),
+            ],
+          ),
+          child: Stack(
             children: [
-              Expanded(
+              Positioned(
+                top: 14,
+                left: 14,
+                child: _DecorationBubble(
+                  text: visualTheme.decorations[0],
+                  size: 42,
+                ),
+              ),
+              Positioned(
+                top: 22,
+                right: 14,
+                child: _DecorationBubble(
+                  text: visualTheme.decorations[1],
+                  size: 38,
+                ),
+              ),
+              Positioned(
+                bottom: 92,
+                right: 18,
+                child: _DecorationBubble(
+                  text: visualTheme.decorations[2],
+                  size: 34,
+                ),
+              ),
+              Positioned(
+                top: 16,
+                right: 16,
                 child: Container(
-                  width: double.infinity,
+                  height: 42,
+                  width: 42,
                   decoration: BoxDecoration(
-                    color: themeColor.withValues(alpha: 0.14),
-                    borderRadius: BorderRadius.circular(24),
+                    color: Colors.white.withValues(alpha: 0.28),
+                    shape: BoxShape.circle,
                   ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      BuddyRiveView(assetPath: buddy.riveAsset),
-                      Positioned(
-                        top: 10,
-                        right: 10,
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                          ),
-                          child: Text(
-                            buddy.emoji,
-                            style: const TextStyle(fontSize: 22),
-                          ),
+                  child: Icon(visualTheme.icon, color: Colors.white, size: 24),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(14, 18, 14, 14),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withValues(alpha: 0.22),
+                          borderRadius: BorderRadius.circular(28),
+                        ),
+                        child: BuddyRiveView(
+                          assetPath: buddy.riveAsset,
+                          fit: rive.Fit.contain,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 12),
-              Text(
-                buddy.name,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              const SizedBox(height: 4),
-              Text(
-                buddy.title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey.shade700,
-                  fontWeight: FontWeight.w600,
+                    ),
+                    const SizedBox(height: 12),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 12,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            buddy.name,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w900,
+                              color: visualTheme.primaryColor,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            buddy.title,
+                            textAlign: TextAlign.center,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey.shade700,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -78,19 +138,25 @@ class BuddyHomeCard extends StatelessWidget {
       ),
     );
   }
+}
 
-  Color get _themeColor {
-    switch (buddy.id) {
-      case 'study_buddy':
-        return Colors.blue;
-      case 'play_buddy':
-        return Colors.orange;
-      case 'learn_buddy':
-        return Colors.purple;
-      case 'activity_buddy':
-        return Colors.green;
-      default:
-        return Colors.teal;
-    }
+class _DecorationBubble extends StatelessWidget {
+  final String text;
+  final double size;
+
+  const _DecorationBubble({required this.text, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.white.withValues(alpha: 0.24),
+        shape: BoxShape.circle,
+      ),
+      child: Text(text, style: TextStyle(fontSize: size * 0.48)),
+    );
   }
 }
